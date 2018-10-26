b1b475779351c2c74127d57c16793818b2b607d9
Daniel <devidra87@gmail.com>
2018-10-26 12:43:45 -0700

    scripts added so page will now accept user input and output beeps boops and 'im Sorry...' message

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
-var beep-boop = function(userNumber){
+//back End Code
+var beepBoop = function(userNumber){
 
+  numbersLoop = [];
 
+  for(x = 0; x <= userNumber; x++) {
+    numbersLoop.push(x.toString());
+  }
+
+  var newArray = numbersLoop.map(function(index) {
+    if (index % 3 == 0){
+      return "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (index.includes('1')){
+      return "Boop!";
+    } else if (index.includes('0')){
+      return "Beep!";
+    } else {
+      return index;
+    }
+});
+
+return newArray
 }
+
+alert(beepBoop(10))
 
 //Front End Code
 $(document).ready(function(){
   $("#speakto").submit(function(event){
-    var
+    var input = $("#userInput").val();
+    $("#reply").text(beepBoop(input));
 
-
- });
+  event.preventDefault();
+  });
 });
